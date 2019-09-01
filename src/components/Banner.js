03db1a48e249fd9b605258deaab6eeefaed2ba62
Charlie Russo <cparusso@gmail.com>
2019-09-01 15:14:41 -0400

    Change how the banner is organized as well as add some lighter colors to the styling

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,46 +2,54 @@ import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, NavLink, Switch } from "react-router-dom";
 
 class Banner extends Component {
-  state={
-    mouseOver: false
-  }
+	state = {
+		mouseOver: false
+	}
 
-  handleMouseEnter = () => {
-    this.setState({
-      mouseOver: true
-    })
-  }
+	handleMouseEnter = () => {
+		this.setState({
+			mouseOver: true
+		})
+	}
 
-  handleMouseLeave = () => {
-    this.setState({
-      mouseOver: false
-    })
-  }
+	handleMouseLeave = () => {
+		this.setState({
+			mouseOver: false
+		})
+	}
 
-  render() {
-    return (
-      <div className="banner-container" >
-        { this.state.mouseOver
-					? <div onMouseLeave={() => this.handleMouseLeave()} className="banner" >
-							<h1>
-								<a className="navbar-link" href="https://www.linkedin.com/in/charlieprusso/" target="blank" >linkedin.</a>
-							</h1>
-							<h1>
-								<a className="navbar-link" href="https://www.github.com/cparusso/" target="blank" >github.</a>
-							</h1>
-							<h1>
-								<a className="navbar-link" href="https://medium.com/@cparusso" target="blank" >medium.</a>
-							</h1>
-						</div> 
-					: <div className="banner" >
-							<h1 onMouseEnter={() => this.handleMouseEnter()} >
-								charlie.
-							</h1>
-						</div>
-        }
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div className="banner-container" >
+				<div 
+					onMouseEnter={() => this.handleMouseEnter()}
+					onMouseLeave={() => this.handleMouseLeave()}
+					style={{ opacity: this.state.mouseOver ? 0 : 1, display: this.state.mouseOver ? 'none' : 'flex' }}
+					className="banner"
+				>
+					<h1 >
+						charlie.
+					</h1>
+				</div>
+				<div
+					onMouseEnter={() => this.handleMouseEnter()}
+					onMouseLeave={() => this.handleMouseLeave()}
+					style={{ opacity: this.state.mouseOver ? 1 : 0, display: this.state.mouseOver ? 'flex' : 'none' }}
+					className="banner"
+				>
+					<h1>
+						<a className="navbar-link" href="https://www.linkedin.com/in/charlieprusso/" target="blank" >linkedin.</a>
+					</h1>
+					<h1>
+						<a className="navbar-link" href="https://www.github.com/cparusso/" target="blank" >github.</a>
+					</h1>
+					<h1>
+						<a className="navbar-link" href="https://medium.com/@cparusso" target="blank" >medium.</a>
+					</h1>
+				</div>
+			</div>
+		)
+	}
 }
 
 export default Banner
