66acadf3b4b81aa7cad5541ef6c03998e5016e4e
Charlie Russo <cparusso@gmail.com>
2019-09-03 18:07:44 -0400

    Add Flickity

--- src/containers/Projects.js
+++ src/containers/Projects.js
@@ -1,17 +1,55 @@
 import React from 'react'
 import Project from '../components/Project.js'
 import Footer from '../components/Footer.js'
+import Flickity from 'react-flickity-component'
 
 import { FaLinkedin, FaGithubSquare, FaMedium } from 'react-icons/fa'
 
 function Projects() {
+
+    const flickityOptions = {
+        initialIndex: 0,
+        autoPlay: true,
+    }
+
     return (
         <div>
-            <div>
-                <Project />
-                <Project />
-                <Project />
-            </div>
+            <Flickity
+                className={'carousel'} // default ''
+                elementType={'div'} // default 'div'
+                options={flickityOptions} // takes flickity options {}
+                disableImagesLoaded={false} // default false
+                reloadOnUpdate // default false
+                static // default false
+        >
+                <div style={{display: "flex", width: "50vw"}}>
+                    <p>
+                        I'm a full-stack software engineer based in New York City with a 
+                        drive to learn and share new technologies and ideas.
+                        I'm a full-stack software engineer based in New York City with a 
+                        drive to learn and share new technologies and ideas.
+                    </p>
+                    <img src="mebab.png" />
+                </div>
+                <div style={{ display: "flex", width: "50vw" }}>
+                    <p>
+                        I'm a full-stack software engineer based in New York City with a
+                        drive to learn and share new technologies and ideas.
+                        I'm a full-stack software engineer based in New York City with a
+                        drive to learn and share new technologies and ideas.
+                    </p>
+                    <img src="mebab.png" />
+                </div>
+                <div style={{ display: "flex", width: "50vw" }}>
+                    <p>
+                        I'm a full-stack software engineer based in New York City with a
+                        drive to learn and share new technologies and ideas.
+                        I'm a full-stack software engineer based in New York City with a
+                        drive to learn and share new technologies and ideas.
+                    </p>
+                    <img src="mebab.png" />
+                </div>
+            </Flickity>
             <Footer />
         </div>
     )
