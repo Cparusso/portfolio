f9bd7a36377e7c63052970ffeb1f59549d3e5d5f
Charlie Russo <cparusso@gmail.com>
2019-09-12 00:01:43 -0400

    Styled mobile nav-menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route, NavLink, Switch } from "react-router-do
 class NavBar extends Component {
 	state = {
 		selected: "home",
+ 		menuOpen: false
 	}
 
 	handleClick = (e) => {
@@ -17,53 +18,101 @@ class NavBar extends Component {
 				selected: "home"
 			})
 		}
+
+		this.toggleMenu()
 	}
+
+	toggleMenu = () => {
+ 		this.setState((prevState) => ({
+ 			menuOpen: !prevState.menuOpen
+ 		}))
+ 	}
+
 
 	render() {
 		return (
-			<div className="navbar fade-in">
-				<div className="nav-left" >
-					<NavLink to="/" className="navbar-link" onClick={ this.handleClick } >
-						<img className={this.state.selected === "home" ? "signature-invert" : "signature"} src="signature.png" />
-					</NavLink>
+			<div>
+				<div className="mobile-hide">
+					<div className="navbar fade-in">
+						<div className="nav-left" >
+							<NavLink to="/" className="navbar-link" onClick={ this.handleClick } >
+								<img className={this.state.selected === "home" ? "signature-invert" : "signature"} src="signature.png" />
+							</NavLink>
+						</div>
+						<div className="nav-center" >
+							<NavLink to="/projects" className={this.state.selected === "portfolio" ? "selected-nav" : "navbar-link"} onClick={this.handleClick} >
+								portfolio
+							</NavLink>
+							<NavLink to="/about" className={this.state.selected === "about" ? "selected-nav" : "navbar-link"} onClick={this.handleClick} >
+								about
+							</NavLink>
+							<a 
+								href="https://drive.google.com/file/d/18-BvMGQThoh5jobPE5phGSw-WJMzS2wJ/view"
+								className="navbar-link"
+								target="blank"
+							>
+								resume
+							</a>
+						</div>
+						<div className="nav-right" >
+							<a href="mailto:hello@charlierusso.me" className="navbar-link" >
+								contact
+							</a>
+						</div>
+						<div className="nav-menu desktop-hide" >
+							<NavLink to="/projects" className={this.state.selected === "portfolio" ? "selected-nav" : "navbar-link"} onClick={this.handleClick} >
+								portfolio
+							</NavLink>
+							<NavLink to="/about" className={this.state.selected === "about" ? "selected-nav" : "navbar-link"} onClick={this.handleClick} >
+								about
+							</NavLink>
+							<a 
+								href="https://drive.google.com/file/d/18-BvMGQThoh5jobPE5phGSw-WJMzS2wJ/view"
+								className="navbar-link"
+								target="blank"
+							>
+								resume
+							</a>
+							<a href="mailto:hello@charlierusso.me" className="navbar-link" >
+								contact
+							</a>
+						</div>
+					</div>
 				</div>
-				<div className="nav-center mobile-hide" >
-					<NavLink to="/projects" className={this.state.selected === "portfolio" ? "selected-nav" : "navbar-link"} onClick={this.handleClick} >
-						portfolio
-					</NavLink>
-					<NavLink to="/about" className={this.state.selected === "about" ? "selected-nav" : "navbar-link"} onClick={this.handleClick} >
-						about
-					</NavLink>
-					<a 
-						href="https://drive.google.com/file/d/18-BvMGQThoh5jobPE5phGSw-WJMzS2wJ/view"
-						className="navbar-link"
-						target="blank"
+				<div className="desktop-hide">
+					<button
+						onClick={this.toggleMenu}
+						className={this.state.menuOpen ? "active hamburger hamburger-cancel desktop-hide" : "hamburger hamburger-cancel desktop-hide"}
 					>
-						resume
-					</a>
-				</div>
-				<div className="mobile-hide" >
-					<a href="mailto:hello@charlierusso.me" className="navbar-link" >
-						contact
-					</a>
-				</div>
-				<div className="nav-menu desktop-hide" >
-					<NavLink to="/projects" className={this.state.selected === "portfolio" ? "selected-nav" : "navbar-link"} onClick={this.handleClick} >
-						portfolio
-					</NavLink>
-					<NavLink to="/about" className={this.state.selected === "about" ? "selected-nav" : "navbar-link"} onClick={this.handleClick} >
-						about
-					</NavLink>
-					<a 
-						href="https://drive.google.com/file/d/18-BvMGQThoh5jobPE5phGSw-WJMzS2wJ/view"
-						className="navbar-link"
-						target="blank"
-					>
-						resume
-					</a>
-					<a href="mailto:hello@charlierusso.me" className="navbar-link" >
-						contact
-					</a>
+						<span className="icon"></span>
+					</button>
+					<div className={this.state.menuOpen ? "nav-menu show-menu" : "nav-menu hide-menu"}>
+						<div className="menu-col">
+							<NavLink to="/" className={this.state.selected === "home" ? "selected-nav" : "navbar-link"} onClick={this.handleClick} >
+								home
+							</NavLink>
+							<NavLink to="/projects" className={this.state.selected === "portfolio" ? "selected-nav" : "navbar-link"} onClick={this.handleClick} >
+								portfolio
+							</NavLink>
+						</div>
+						<div className="menu-col">
+							<NavLink to="/about" className={this.state.selected === "about" ? "selected-nav" : "navbar-link"} onClick={this.handleClick} >
+								about
+							</NavLink>
+							<a
+								href="https://drive.google.com/file/d/18-BvMGQThoh5jobPE5phGSw-WJMzS2wJ/view"
+								className="navbar-link"
+								target="blank"
+								>
+								resume
+							</a>
+						</div>
+						<div className="menu-col">
+							<a href="mailto:hello@charlierusso.me" className="navbar-link" >
+								contact
+							</a>
+						</div>
+					</div>
 				</div>
 			</div>
 		)
