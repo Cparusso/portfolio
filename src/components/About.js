b73aa4810b1555176913b0fc32808b43cec306fe
Charlie Russo <cparusso@gmail.com>
2019-12-11 10:23:09 -0500

    Update about me page to include specific technologies used.

--- src/components/About.js
+++ src/components/About.js
@@ -8,15 +8,10 @@ function About() {
                 <div className="banner-about fade-in" >
                     <h1 className="about-header" >Hi, I'm Charlie.</h1>
                     <p>
-                        I'm a full-stack software engineer
-                        based in New York City. I graduated from Flatiron School in
-                        December of 2018 and have been working there in an instructional
-                        role since then.
+                        I'm a full-stack software engineer based in New York City. I graduated from Flatiron School in December of 2018 and have been working in an instructional role since.
                     </p>
                     <p>
-                        I've worked directly with over 100 students, to learn how to debug their code, 
-                        overcome code challenges, and collaborate with other developers on creative
-                        and inventive projects.
+                        I've worked directly with over 100 students, teaching them how to debug their code, overcome code challenges, and collaborate with other developers on creative and inventive projects with Javascript, React, Redux, Ruby, Rails and React Native.
                     </p>
                 </div>
                 <img className="mobile-hide picture-of-menobeard fade-in" src="menobeard.png" alt="charlie-crossed-arms-img"/>
